b76d03035482537c0336d620e072e28b80a8a85b
ivarejao <imsvarejao@gmail.com>
2021-03-31 13:38:15 -0300

    Adicionado Cores e Pdf

---
+++ src/Eleicoes/Cores.java
@@ -0,0 +1,14 @@
+package Eleicoes;
+
+public class Cores {
+
+    public static String RED = "\033[31m";
+    public static String GREEN = "\033[32m";
+    public static String YELLOW = "\033[33m";
+    public static String BLUE = "\033[34m";
+    public static String MAGENTA = "\033[35m";
+    public static String CYAN = "\033[36m";
+    public static String WHITE = "\033[37m";
+    public static String RESET = "\033[0m";
+
+}
