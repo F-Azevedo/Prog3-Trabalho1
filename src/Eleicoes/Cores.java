be482e6bf1a911a6aaa55603def2a8f42a5dd42a
Fernando Azevedo <fernando.azevedo.peres@gmail.com>
2021-04-03 01:12:53 -0300

    Comentários, JavaDoc do Cores.java
    Fiz pq para gerar o JavaDoc sem erros todas as funções precisam ter uma descrição.

--- src/Eleicoes/Cores.java
+++ src/Eleicoes/Cores.java
@@ -1,14 +1,43 @@
 package Eleicoes;
 
+/**
+ * Classe para dar cor a textos.
+ */
 public class Cores {
 
+    /**
+     * Vermelho.
+     */
     public static String RED = "\033[31m";
+
+    /**
+     * Verde.
+     */
     public static String GREEN = "\033[32m";
+
+    /**
+     * Amarelo.
+     */
     public static String YELLOW = "\033[33m";
+
+    /**
+     * Azul.
+     */
     public static String BLUE = "\033[34m";
+
+    /**
+     * Magenta.
+     */
     public static String MAGENTA = "\033[35m";
+
+    /**
+     * Ciano.
+     */
     public static String CYAN = "\033[36m";
-    public static String WHITE = "\033[37m";
+
+    /**
+     * Reseta a cor.
+     */
     public static String RESET = "\033[0m";
 
 }
