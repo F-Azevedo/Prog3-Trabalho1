e44665c5496b221a383d5bdd1600ef50a326a3e9
Fernando Azevedo <fernando.azevedo.peres@gmail.com>
2021-04-03 00:16:29 -0300

    Remoção das funções que não eram necessárias. Comentários, JavaDoc do Candidato.java

--- src/Eleicoes/Candidato.java
+++ src/Eleicoes/Candidato.java
@@ -2,8 +2,10 @@ package Eleicoes;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.Objects;
 
+/**
+ * Classe para trarar coisas relacionadas a um candidato da eleição.
+ */
 public class Candidato implements Comparable<Candidato>{
 
 	private final String nome;
@@ -16,6 +18,18 @@ public class Candidato implements Comparable<Candidato>{
 	private final char sexo;
 	private final LocalDate nasc;
 
+	/**
+	 * Construtor da classe candidato.
+	 * @param numero número do candidato.
+	 * @param votos quantidade de votos recebidos pelo candidato.
+	 * @param status se o candidato foi ou não eleito.
+	 * @param nome nome do candidato.
+	 * @param nome_urna nome de urna do candidato.
+	 * @param sexo sexo do candidato.
+	 * @param nasc data de nascimento do candidato.
+	 * @param num_partido numero do partido ao qual o candidato pertence.
+	 * @param eleicao data de realização da eleição.
+	 */
 	public Candidato(int numero, int votos, String status, String nome, String nome_urna,
 					 char sexo, LocalDate nasc, int num_partido, LocalDate eleicao) {
 		this.nome = nome;
@@ -29,90 +43,74 @@ public class Candidato implements Comparable<Candidato>{
 		this.idade = (int) nasc.until(eleicao, ChronoUnit.YEARS);
 	}
 
+	/**
+	 * Função para verificar se o candidato foi eleito.
+	 * @return True se o candidato foi eleito, falso do contrario.
+	 */
 	public boolean foiEleito() {
 		return this.status.equals("Eleito");
 	}
 
-	// Getters
-	public String getNome() {
-		return nome;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public String getNome_urna() {
-		return nome_urna;
-	}
-
+	/**
+	 * Getter de votos do candidato.
+	 * @return a quantidade de votos do candidato.
+	 */
 	public int getVotos() {
 		return votos;
 	}
 
+	/**
+	 * Getter de idade do candidato.
+	 * @return a idade do candidato.
+	 */
 	public int getIdade() {
 		return idade;
 	}
 
-	public int getNumero() {
-		return numero;
-	}
-
+	/**
+	 * Getter do número do partido do candidato.
+	 * @return o número do partido do candidato.
+	 */
 	public int getNum_partido() {
 		return num_partido;
 	}
 
+	/**
+	 * Getter do sexo do candidato.
+	 * @return o sexo do candidato.
+	 */
 	public char getSexo() {
 		return sexo;
 	}
 
- 	public LocalDate getNasc() {
-		return nasc;
-	}
-
-	// Compara candidatos pelo número de votos nominais de forma decrescente
+	/**
+	 * Função de comparação necessária para inserir na TreeSet de forma ordenada.
+	 * Compara por quantidade de votos.
+	 * @param outro o candidato com o qual sera comparado.
+	 * @return o resultado da comparação.
+	 */
 	@Override
 	public int compareTo(Candidato outro) {
+		//Insere de forma decrescente
 		return Integer.compare(outro.votos, this.votos);
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Candidato candidato = (Candidato) o;
-		return numero == candidato.numero;
+	/**
+	 * Função para imprimir o nome do candidato, seu nome de urna, a sigla do partido e os votos do candidato.
+	 * @param sigla_partido sigla do partido.
+	 * @return retorna a string formatada.
+	 */
+	public String simpleString(String sigla_partido) {
+		return nome + " / " + nome_urna + " (" + sigla_partido + ", " + votos + " votos)";
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(numero);
-	}
-
-
-	public String simpleString(String nome_partido) {
-		return nome + " / " + nome_urna + " (" + nome_partido + ", " + votos + " votos)";
-	}
-
-	// Imprime o candidato de forma mais simplória
+	/**
+	 * toString do candidato, imprime o nome de urna, numero e quantidade de votos.
+	 * @return a string formatada.
+	 */
 	@Override
 	public String toString() {
 		return nome_urna + " (" + numero + ", " + votos + " votos)";
 	}
-
-	// Método que imprime todas as informações do candidato
-	public String detailedString(){
-		String space ="           ";
-		return "Candidato: nome= " + nome + ",\n" +
-				space + "nome_urna= " + nome_urna + ",\n" +
-				space + "numero= " + numero + ",\n" +
-				space + "num_partido= " + num_partido + ",\n" +
-				space + "status= " + status + ",\n" +
-				space + "sexo= " + sexo + ",\n" +
-				space + "nasc= " + nasc + ",\n" +
-				space + "votos= " + votos + ",\n" +
-				space + "idade= " + idade + "\n\n";
-	}
-
 }
 
