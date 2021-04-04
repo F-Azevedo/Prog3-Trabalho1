dd1bf1c463cc7cfdb59659fc0f8d4907a075cbf3
Fernando Azevedo <fernando.azevedo.peres@gmail.com>
2021-04-04 13:11:09 -0300

    Diretório com o JavaDoc feito.

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Eleicoes"},{"l":"Entrada"},{"l":"Saida"}];updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"Eleicoes","l":"Candidato"},{"p":"Eleicoes","l":"comparaMaisVotado"},{"p":"Eleicoes","l":"Cores"},{"p":"Eleicoes","l":"Eleicao"},{"p":"Saida","l":"Estatisticas"},{"p":"Entrada","l":"Leitor"},{"p":"Eleicoes","l":"Partido"},{"p":"Eleicoes","l":"Testador"}];updateSearchResults();
